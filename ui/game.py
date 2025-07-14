ff5e045ac566d30a523dafc229f17f416ea911ea
AdamDavis08 <hristijangajdov@gmail.com>
2025-07-14 17:37:50 +0200

    Initial Board

---
+++ ui/game.py
@@ -0,0 +1,63 @@
+import pygame
+from colors import *
+
+pygame.init()
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+TRIANGLE_WIDTH = SCREEN_WIDTH / 15
+TRIANGLE_HEIGHT = SCREEN_HEIGHT * 0.4
+BOARD_HEIGHT = 20
+BOARD_WIDTH = TRIANGLE_WIDTH
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Gamonix")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    screen.fill(BOARD_BACKGROUND)
+    
+    is_dark = True
+    for y in (0, SCREEN_HEIGHT):
+        is_dark = not is_dark
+        for x in range(1, 15):
+            if x == 7: continue
+            
+            points = []
+            p1 = x * TRIANGLE_WIDTH
+            p2 = (x + 1) * TRIANGLE_WIDTH
+            p3 = (2 * x + 1) / 2 * TRIANGLE_WIDTH
+            
+            mult = -1 if y else 1
+            color = DARK_TRIANGLES if is_dark else LIGHT_TRIANGLES
+            is_dark = not is_dark
+            
+            points.append((p1, y))
+            points.append((p2, y))
+            points.append((p3, y + mult * TRIANGLE_HEIGHT))
+            pygame.draw.polygon(screen, color, points)
+    
+    #Borders
+    pygame.draw.rect(screen, BOARD_BORDER, (0, 0, SCREEN_WIDTH, BOARD_HEIGHT))
+    pygame.draw.rect(screen, BOARD_BORDER, (0, SCREEN_HEIGHT - BOARD_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.draw.rect(screen, BOARD_BORDER, (0, 0, BOARD_WIDTH, SCREEN_HEIGHT))
+    pygame.draw.rect(screen, BOARD_BORDER, (SCREEN_WIDTH - BOARD_WIDTH, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #Middle Border
+    pygame.draw.rect(screen, BOARD_BORDER, (SCREEN_WIDTH / 2 - BOARD_WIDTH / 2, 0, BOARD_WIDTH, SCREEN_HEIGHT))
+    
+    #Off sections
+    OFF_SECTION_WIDTH = BOARD_WIDTH - 2 * BOARD_HEIGHT
+    OFF_SECTION_HEIGHT = 20
+    pygame.draw.rect(screen, OFF_SECTION, (BOARD_HEIGHT, BOARD_HEIGHT, OFF_SECTION_WIDTH, TRIANGLE_HEIGHT + OFF_SECTION_HEIGHT))
+    pygame.draw.rect(screen, OFF_SECTION, (BOARD_HEIGHT, SCREEN_HEIGHT * 0.6 - OFF_SECTION_HEIGHT - BOARD_HEIGHT, OFF_SECTION_WIDTH, TRIANGLE_HEIGHT + OFF_SECTION_HEIGHT))
+    pygame.draw.rect(screen, OFF_SECTION, (SCREEN_WIDTH - BOARD_HEIGHT - OFF_SECTION_WIDTH, BOARD_HEIGHT, OFF_SECTION_WIDTH, TRIANGLE_HEIGHT + OFF_SECTION_HEIGHT))
+    pygame.draw.rect(screen, OFF_SECTION, (SCREEN_WIDTH - BOARD_HEIGHT - OFF_SECTION_WIDTH, SCREEN_HEIGHT * 0.6 - OFF_SECTION_HEIGHT - BOARD_HEIGHT, OFF_SECTION_WIDTH, TRIANGLE_HEIGHT + OFF_SECTION_HEIGHT))
+    pygame.display.flip()
+
+pygame.quit()
